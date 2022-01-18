0dcde8834ba94cf22de712bd4e12061ebf6f2683
Ashwith Poojary <pashwith25@gmail.com>
2022-01-18 12:12:14 +0530

    fixed firebase

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,32 +1,98 @@
-import React from "react";
-import { View, Text,StyleSheet } from "react-native";
-import {  Icon, Fab } from "native-base";
-import { Ionicons } from '@expo/vector-icons';
+import React, { Component } from "react";
 
-const HomeScreen = ({navigation}) => {
+import { StyleSheet, ScrollView, ActivityIndicator, View } from "react-native";
+import { ListItem } from "react-native-elements";
+import * as firebase from "firebase";
+import { Icon, Fab } from "native-base";
+import { Ionicons } from "@expo/vector-icons";
+
+class HomeScreen extends Component {
+  constructor() {
+    super();
+    this.docs = firebase.firestore().collection("passwords");
+    this.state = {
+      isLoading: true,
+      creds: [],
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.docs.onSnapshot(this.getCredsData);
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  getCredsData = (querySnapshot) => {
+    const creds = [];
+    querySnapshot.forEach((res) => {
+      const data = res.data().password;
+      creds.push({
+        key: res.id,
+        data,
+      });
+    });
+    this.setState({
+      creds,
+      isLoading: false,
+    });
+  };
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color="red" />
+        </View>
+      );
+    }
     return (
-            <View style={styles.container}>
-                <Text>Home</Text>
-            
-            <Fab
-                placement="bottom-right"
-                colorScheme="blue"
-                size="lg"
-                icon={<Icon as={Ionicons} name="add" />}
-                renderInPortal = {false}
-                onPress={() => navigation.navigate('New')}
-            />
-            </View>
+      <View style={styles.container}>
+        <ScrollView style={styles.wrapper}>
+          {this.state.creds.map((res, i) => {
+            return (
+              <ListItem key={i} bottomDivider>
+                <ListItem.Content>
+                  <ListItem.Title>{res.key}</ListItem.Title>
+                  <ListItem.Subtitle>{res.data}</ListItem.Subtitle>
+                </ListItem.Content>
+              </ListItem>
+            );
+          })}
+        </ScrollView>
+        <Fab
+          placement="bottom-right"
+          colorScheme="blue"
+          size="lg"
+          icon={<Icon as={Ionicons} name="add" />}
+          renderInPortal={false}
+          onPress={() => navigation.navigate("New")}
+        />
+      </View>
     );
-};
+  }
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },  
+  container: {
+    flex: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  wrapper: {
+    flex: 1,
+    paddingBottom: 22,
+  },
+  loader: {
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
 });
-
 
 export default HomeScreen;
